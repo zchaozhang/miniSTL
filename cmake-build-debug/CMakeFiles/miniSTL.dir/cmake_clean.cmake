bfb5b7f33da444f9e527811ef804aa88bb3073e7
张智超 <zhangzhichao@macbook-2.local>
2018-02-11 00:32:39 +0800

    cmake

---
+++ cmake-build-debug/CMakeFiles/miniSTL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniSTL.dir/main.cpp.o"
+  "miniSTL.pdb"
+  "miniSTL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miniSTL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
